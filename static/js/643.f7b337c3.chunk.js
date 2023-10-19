ba793cb6343cb06257b0e65ff82dfc7cb838ddee
yevgeniy8 <116801272+yevgeniy8@users.noreply.github.com>
2023-10-19 22:41:53 +0000

    Deploying to gh-pages from @ yevgeniy8/car-rent-agency@789f8970925843aa49294dbe38f1599620903506 🚀

--- static/js/643.f7b337c3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[643],{643:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Favorite"})}}}]);
-//# sourceMappingURL=643.f7b337c3.chunk.js.map
